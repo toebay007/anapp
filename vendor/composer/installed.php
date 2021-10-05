dcb84f1b567f9e6ba46ccce11e208edf77001e9f
toebay007 <toebay007@gmail.com>
2021-10-05 04:21:56 +0100

    x

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '428359b86cfaa3ca2b8adc3d87922887d1c898cb',
+    'name' => 'toebay007/php-wallet-devplacement',
+  ),
+  'versions' => 
+  array (
+    'toebay007/php-wallet-devplacement' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '428359b86cfaa3ca2b8adc3d87922887d1c898cb',
+    ),
+  ),
+);
